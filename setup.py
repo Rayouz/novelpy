534a54ea9272a4d4810c78660a0bc30a1d0d1255
Florence Blandinieres <florence.blandinieres@technopolis-group.com>
2024-02-08 17:02:38 +0100

    update of sklearn
    Update of the library sklearn==1.2.1 that is no longer maintained -> scikit-learn

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
- 
+
 classifiers = [
   'Development Status :: 5 - Production/Stable',
   'Intended Audience :: Education',
@@ -7,20 +7,20 @@ classifiers = [
   'License :: OSI Approved :: MIT License',
   'Programming Language :: Python :: 3'
 ]
- 
+
 setup(
   name='novelpy',
   version='1.3',
   description='Calculate novelty indicators',
   long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
-  url='',  
+  url='',
   author='Pelletier Pierre and Wirtz Kevin',
   author_email='kevin.wirtz@unistra.fr',
-  license='MIT', 
+  license='MIT',
   classifiers=classifiers,
-  keywords='Novelty, scientometrics', 
+  keywords='Novelty, scientometrics',
   packages=find_packages(),
-  install_requires=['pymongo==3.12.1','joblib==1.1.0','sklearn==1.2.1','glob2==0.7','tqdm==4.62.3',"networkx==2.5.1",'python-louvain==0.15',
+  install_requires=['pymongo==3.12.1','joblib==1.1.0','scikit-learn-1.4.0','glob2==0.7','tqdm==4.62.3',"networkx==2.5.1",'python-louvain==0.15',
   'numpy==1.19.5','pickle-mixin==1.0.2','scipy==1.5.4','pandas==1.1.5','multiprocess==0.70.12.2','pyyaml==5.4.1','spacy==3.0.0','scispacy==0.4.0','cycler==0.11.0','thinc==7.4.1',
-  'wosfile==0.5','seaborn==0.11.2'] 
+  'wosfile==0.5','seaborn==0.11.2']
 )
